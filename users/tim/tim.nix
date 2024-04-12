403de79718ac0908c7bd182008914151b25cc4b5
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-12 16:53:58 -0700

    Hyperland basic config in home manager

--- users/tim/tim.nix
+++ users/tim/tim.nix
@@ -9,6 +9,7 @@ let
 in {
   imports = [
     ../home-manager.nix
+    ../../programs/hyprland.nix
   ];
 
   options = {
@@ -38,6 +39,104 @@ in {
           vscodium-fhs
           discord
         ];
+
+        wayland.windowManager.hyprland.enable = lib.mkIf config.tw.programs.hyprland.enable true;
+        wayland.windowManager.hyprland.settings = lib.mkIf config.tw.programs.hyprland.enable {
+          monitor = ",preferred,auto,1,vrr,1";
+          "$terminal" = "kitty";
+          env = [
+            "XCURSOR_SIZE,24"
+            "QT_QPA_PLATFORMTHEME,qt6ct"
+          ];
+          input = {
+            kb_layout = "us";
+            follow_mouse = 1;
+            touchpad = {
+              natural_scroll = false;
+            };
+          };
+          general = {
+            gaps_in = 5;
+            gaps_out = 20;
+            border_size = 2;
+            "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+            "col.inactive_border" = "rgba(595959aa)";
+            layout = "dwindle";
+          };
+          decoration = {
+            rounding = 10;
+            blur = {
+              enabled = true;
+              size = 3;
+              passes = 1;
+            };
+
+            drop_shadow = true;
+            shadow_range = 4;
+            shadow_render_power = 3;
+            "col.shadow" = "rgba(1a1a1aee)";
+          };
+          animations = {
+            enabled = true;
+            bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+            animation = [
+              "windows, 1, 7, myBezier"
+              "windowsOut, 1, 7, default, popin 80%"
+              "border, 1, 10, default"
+              "borderangle, 1, 8, default"
+              "fade, 1, 7, default"
+              "workspaces, 1, 6, default"
+            ];
+          };
+          dwindle = {
+            pseudotile = true;
+            preserve_split = true;
+          };
+          master = {
+            new_is_master = true;
+          };
+          gestures = {
+            workspace_swipe = true;
+          };
+          misc = {
+            force_default_wallpaper = -1;
+          };
+
+          "$mod" = "SUPER";
+          bind = [
+            "$mod, Return, exec, $terminal"
+            "$mod, F4, killactive,"
+            "$mod, Q, exit,"
+            "$mod, E, exec, $fileManager"
+            "$mod, F, togglefloating,"
+            "$mod, R, exec, $menu"
+
+            "$mod, left, movefocus, l"
+            "$mod, right, movefocus, r"
+            "$mod, up, movefocus, u"
+            "$mod, down, movefocus, d"
+          ] ++ (
+            # workspaces
+            # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+            builtins.concatLists (builtins.genList (
+                x: let
+                  ws = builtins.toString (x + 1);
+                  key = builtins.toString (if x < 10 then x + 1 else 0);
+                in [
+                  "$mod, ${ws}, workspace, ${ws}"
+                  "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
+                ]
+              )
+              10)
+          );
+
+          bindm = [
+            "$mod, mouse:272, movewindow"
+            "$mod, mouse:273, resizewindow"
+            "$mod SHIFT, mouse:272, resizewindow"
+          ];
+        };
 
         programs.git = {
           enable = true;
