dd3bbd2385847a05bb28bc0a3ff070464c8b3462
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-11 20:12:12 -0700

    More hyprland setup

--- machines/framework/configuration.nix
+++ machines/framework/configuration.nix
@@ -28,7 +28,7 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   
 
--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -20,5 +20,40 @@ in {
 
   config = lib.mkIf cfg.enable {
     programs.hyprland.enable = true;
+    environment.sessionVariables = {
+      # Try to get Electron apps to use Wayland
+      NIXOS_OZONE_WL = "1";
+    };
+
+    # For screen sharing
+    services.dbus.enable = true;
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+    };
+
+    # Waybar
+    nixpkgs.overlays = [
+      (self: super: {
+        waybar = super.waybar.overrideAttrs (oldAttrs: {
+          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        });
+      })
+    ];
+
+    environment.systemPackages = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xwayland
+      meson
+      wayland-protocols
+      wayland-utils
+      wl-clipboard
+      wlroots
+      pavucontrol
+    ];
+
+    # Use sddm display manager
+    services.displayManager.sddm.enable = true;
+    services.xserver.enable = true;
   };
 }
