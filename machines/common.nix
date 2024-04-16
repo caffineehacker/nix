76f607e5b13124d4b658a7a8a969576d4cd17e26
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-16 10:34:10 -0700

    Add garbage collection

--- machines/common.nix
+++ machines/common.nix
@@ -3,4 +3,9 @@
   time.timeZone = "America/Los_Angeles";
   i18n.defaultLocale = "en_US.UTF-8";
   nixpkgs.config.allowUnfree = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
