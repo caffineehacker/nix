b9fe07ecbdce3bda3ef462a731e32a30c0a14a65
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-18 15:22:45 -0700

    Fix update script so errors can be seen

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nh
+set -e
 
 nh os switch --ask --update $(dirname "$0")
+echo "Press enter to continue..."
+read
 nh clean all --ask --keep 5 --keep-since 30d
