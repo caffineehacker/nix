55b9988356095b315ece2cd3e1b6cef541b10614
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-18 12:53:39 -0700

    Switch to using nh instead of nvd directly for updates

--- machines/common.nix
+++ machines/common.nix
@@ -13,6 +13,5 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    nvd
   ];
 }
--- update.sh
+++ update.sh
@@ -1,13 +1,5 @@
-#!/usr/bin/env bash
-set -e
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nh
 
-nix flake update
-nixos-rebuild boot --flake . --upgrade-all
-nvd diff /var/run/current-system /nix/var/nix/profiles/system-$(nix-env --list-generations -p /nix/var/nix/profiles/system | tail -n1 | grep -Po "\d+" | head -n1)-link
-echo "Press enter to continue..."
-read
-/nix/var/nix/profiles/system-$(nix-env --list-generations -p /nix/var/nix/profiles/system | tail -n1 | grep -Po "\d+" | head -n1)-link/bin/switch-to-configuration switch
+nh os switch --ask --update $(dirname "$0")
+nh clean all --ask --keep 5 --keep-since 30d
