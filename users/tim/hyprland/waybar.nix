691b225a323c4019c7a6dbea9667c1ccac26d09c
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-26 16:54:03 -0700

    Use nix colors for waybar

--- users/tim/hyprland/waybar.nix
+++ users/tim/hyprland/waybar.nix
@@ -44,7 +44,6 @@
             };
 
             "custom/power_btn" = {
-              # TODO make this a menu of options
               format = "";
               on-click = "sh -c '(sleep 0.5s; wlogout --protocol layer-shell)' & disown";
               tooltip = false;
@@ -150,7 +149,117 @@
             };
           };
         };
-        style = ./hyprv4/waybar/style/v4-style-dark.css;
+        style = ''
+* {
+    border: none;
+    border-radius: 0;
+    font-family: "JetBrainsMono Nerd Font";
+    font-weight: bold;
+    font-size: 16px;
+    min-height: 0;
+}
+
+window#waybar {
+    opacity: 1;
+    background: #${config.tw.users.tim.colorScheme.palette.base00};
+    color: #${config.tw.users.tim.colorScheme.palette.base05};
+}
+
+tooltip {
+    background: #${config.tw.users.tim.colorScheme.palette.base01};
+    opacity: 0.8;
+    border-radius: 10px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #${config.tw.users.tim.colorScheme.palette.base08};
+}
+
+tooltip label{
+    color: #${config.tw.users.tim.colorScheme.palette.base04};
+}
+
+#taskbar button {
+    color: #${config.tw.users.tim.colorScheme.palette.base05};
+}
+
+#workspaces button {
+    padding: 5px;
+    color: #${config.tw.users.tim.colorScheme.palette.base05};
+    margin-right: 5px;
+}
+
+#workspaces button.active {
+    color: #000000;
+    background: #${config.tw.users.tim.colorScheme.palette.base02};
+    border-radius: 10px;
+}
+
+#workspaces button:hover {
+    background: #${config.tw.users.tim.colorScheme.palette.base00};
+    color: #${config.tw.users.tim.colorScheme.palette.base04};
+    border-radius: 10px;
+}
+
+#custom-launch_wofi,
+#custom-launch_firefox,
+#custom-launch_thunderbird,
+#custom-launch_thunar,
+#custom-launch_kitty,
+#custom-lock_screen,
+#custom-light_dark,
+#custom-power_btn,
+#custom-power_profile,
+#custom-weather,
+#custom-myhyprv,
+#window,
+#cpu,
+#disk,
+#custom-updates,
+#memory,
+#clock,
+#battery,
+#pulseaudio,
+#network,
+#tray,
+#temperature,
+#workspaces,
+#idle_inhibitor,
+#backlight {
+    background: #${config.tw.users.tim.colorScheme.palette.base00};
+    opacity: 1;
+    padding: 0px 8px;
+    margin: 0px 3px;
+    border: 0px;
+}
+
+#workspaces {
+    padding-right: 0px;
+    padding-left: 5px;
+}
+
+#window {
+    border-radius: 10px;
+    margin-left: 0px;
+    margin-right: 0px;
+}
+
+#custom-launch_firefox, 
+#custom-launch_thunderbird,
+#custom-launch_thunar,
+#custom-launch_wofi,
+#custom-launch_kitty,
+#custom-weather {
+    margin-left: 0px;
+    border-right: 0px;
+    font-size: 24px;
+    margin-right: 20px;
+}
+
+#custom-launch_firefox, 
+#custom-launch_kitty {
+    font-size: 20px;
+}
+        '';
       };
 
       programs.wlogout.enable = true;
