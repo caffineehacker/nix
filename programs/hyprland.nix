6380aa380cb2f3b4a345653be8fc19aafde3807c
Tim Waterhouse <tim@timwaterhouse.com>
2024-04-30 10:51:32 -0700

    Fix sddm on Wayland

--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -56,6 +56,7 @@ in
 
     # Use sddm display manager
     services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
     services.xserver.enable = true;
   };
 }
