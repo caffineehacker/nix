aaacfab8e3289633aaba014257f26e6891449658
Tim Waterhouse <tim@timwaterhouse.com>
2024-05-10 14:49:38 -0700

    Make a fast upgrade script that is the same as the normal one, but with more parallel builds

---
+++ update-fast.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nh
+set -e
+
+# This will get the latest release of hyprland
+sed -i "s/^\(\s*\).*\( ### REPLACE_HYPRLAND_TAG$\)/\1ref = $(curl https://api.github.com/repos/hyprwm/Hyprland/releases/latest | grep tag_name | grep -o ": \".*\"" | cut --complement --characters=1-2);\2/g" flake.nix
+# This will get the latest passing hydra build
+sed -i "s/^\(\s*\).*\( ### REPLACE_NIXPKGS_REV$\)/\1rev = $(./find-nixpkg-rev.sh);\2/g" flake.nix
+nh os switch --ask --update $(dirname "$0") -- --keep-failed --keep-going
+echo "Press enter to continue..."
+read
+nh clean all --ask --keep 5 --keep-since 30d
