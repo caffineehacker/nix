2704a76bb26d92467ebcde8efc119b12330b4eef
Tim Waterhouse <tim@timwaterhouse.com>
2024-05-31 14:39:10 -0700

    Clarify how to add a new sops key

--- update-sops-keys.sh
+++ update-sops-keys.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash sops
 
+# Update .sops.yaml to add keys
+# To produce keys run `nix-shell -p ssh-to-age --run 'ssh-keyscan <server> | ssh-to-age'`
+
 sops updatekeys secrets/*.yaml
