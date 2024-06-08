a2d59bff9cae51c3999f962f71fb26c61148ff5a
Tim Waterhouse <tim@timwaterhouse.com>
2024-06-08 13:02:21 -0700

    Move vaultwarden config out of config.json and add fail2ban

--- containers/vaultwarden.nix
+++ containers/vaultwarden.nix
@@ -35,6 +35,16 @@ in {
         services.vaultwarden = {
           enable = true;
           backupDir = "/var/backup/vaultwarden";
+          config = {
+            DOMAIN = "https://vault.timwaterhouse.com";
+            SIGNUPS_ALLOWED = false;
+            SIGNUPS_VERIFY = true;
+            INVITATION_ORG_NAME = "Vaultwarden";
+            IP_HEADER = "X-Forwarded-For";
+            ROCKET_ADDRESS = "127.0.0.1";
+            ROCKET_PORT = 8222;
+            # TODO: Enable email sending!
+          };
         };
 
         tw.containers.cloudflared = {
@@ -54,6 +64,41 @@ in {
         };
 
         services.resolved.enable = true;
+
+        environment.etc = {
+          "fail2ban/filter.d/vaultwarden.local".text = ''
+            [Definition]
+            failregex = ^.*?Username or password is incorrect\. Try again\. IP: <ADDR>\. Username:.*$
+            backend = systemd
+            journalmatch = _SYSTEMD_UNIT=vaultwarden.service
+          '';
+        };
+        services.fail2ban = {
+          enable = true;
+          # Ban IP after 5 failures
+          maxretry = 5;
+          bantime = "1h";
+          bantime-increment = {
+            # Enable increment of bantime after each violation
+            enable = true;
+            multipliers = "1 2 4 8 16 32 64";
+            # Do not ban for more than 1 week
+            maxtime = "168h";
+            # Calculate the bantime based on all the violations
+            overalljails = true;
+          };
+          ignoreIP = [
+            "10.0.0.0/24"
+            "10.100.0.0/24"
+          ];
+          jails = {
+            vaultwarden.settings = {
+              enabled = true;
+              filter = "vaultwarden";
+              backend = "systemd";
+            };
+          };
+        };
       };
     };
   };
