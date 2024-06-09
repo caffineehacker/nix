8fab0ca82748f46b6baf0cfc81e3f3acbe502beb
Tim Waterhouse <tim@timwaterhouse.com>
2024-06-09 13:50:02 -0700

    Fix wireguard config to remove deprecated option

--- machines/cloud/wireguard.nix
+++ machines/cloud/wireguard.nix
@@ -14,10 +14,8 @@
         };
         wireguardPeers = [
           {
-            wireguardPeerConfig = {
-              PublicKey = "48/1lPw1HEw2mUh7B7x/el/FJIMN7aWdL7MYZ37p0EA=";
-              AllowedIPs = [ "10.100.0.2" ];
-            };
+            PublicKey = "48/1lPw1HEw2mUh7B7x/el/FJIMN7aWdL7MYZ37p0EA=";
+            AllowedIPs = [ "10.100.0.2" ];
           }
         ];
       };
