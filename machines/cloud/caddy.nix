caa026735eef83697be94b6149c5d60b8308e291
Tim Waterhouse <tim@timwaterhouse.com>
2024-06-09 13:50:16 -0700

    Add fail2ban for caddy

--- machines/cloud/caddy.nix
+++ machines/cloud/caddy.nix
@@ -15,4 +15,27 @@
   networking.firewall = {
     allowedTCPPorts = [ 80 443 8448 ];
   };
+
+  services.fail2ban = {
+    enable = true;
+    jails = {
+      caddy-status = {
+        settings = {
+          enabled = true;
+          port = "http,https";
+          filter = "caddy-status";
+          logpath = "/var/log/caddy/*.access.log";
+          maxretry = 10;
+        };
+      };
+    };
+  };
+  # This regex ignores all json and ensures we're not in a string when we find the remote_ip and status fields
+  environment.etc."fail2ban/filter.d/caddy-status.local" = {
+    text = ''
+      [Definition]
+      failregex = ^([^"]|"([^"]|\\")*")*"remote_ip":"<ADDR>"([^"]|"([^"]|\\")*")*"status":(0|403|404)([^"]|"([^"]|\\")*")*$
+      datepattern = LongEpoch
+    '';
+  };
 }
