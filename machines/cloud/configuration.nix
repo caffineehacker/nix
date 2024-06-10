59dfa67b635f0a989ad3fb72942328ba467e3d23
Tim Waterhouse <tim@timwaterhouse.com>
2024-06-09 20:42:13 -0700

    Set default fail2ban settings

--- machines/cloud/configuration.nix
+++ machines/cloud/configuration.nix
@@ -16,7 +16,21 @@
   systemd.network.enable = true;
   networking.networkmanager.enable = false;
   networking.useNetworkd = true;
-  services.fail2ban.enable = true;
+  services.fail2ban = {
+    enable = true;
+    # Ban IP after 5 failures
+    maxretry = 5;
+    bantime = "1h";
+    bantime-increment = {
+      # Enable increment of bantime after each violation
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64";
+      # Do not ban for more than 1 week
+      maxtime = "168h";
+      # Calculate the bantime based on all the violations
+      overalljails = true;
+    };
+  };
 
   services.openssh.enable = true;
 
