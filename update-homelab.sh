d43e6b29803b0a0ea36c1d4f6817ea3989e0de52
Tim Waterhouse <tim@timwaterhouse.com>
2024-06-29 13:58:37 -0700

    Fix equality check in update-homelab.sh

--- update-homelab.sh
+++ update-homelab.sh
@@ -2,7 +2,8 @@
 set -e
 
 HOSTNAME=homelab
-if [[ "$1" -eq "mobile" ]]; then
+if [[ "$1" == "mobile" ]]; then
     HOSTNAME=homelab.timwaterhouse.com
 fi
+echo Using $HOSTNAME
 nixos-rebuild switch --flake .#homelab --use-remote-sudo --target-host $HOSTNAME --build-host $HOSTNAME --keep-failed
