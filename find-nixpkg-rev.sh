93cc95481e8ce34f8b7336017ab6f31a4086c6e1
Tim Waterhouse <tim@timwaterhouse.com>
2024-07-10 10:26:39 -0700

    Remove old comment that no longer applies

--- find-nixpkg-rev.sh
+++ find-nixpkg-rev.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p curl jq
 
-# We use electron since it takes forever to build
 JOBSETEVAL=$(curl -L -H 'Accept: application/json' https://hydra.nixos.org/job/nixpkgs/trunk/unstable/latest-finished | jq .jobsetevals[0])
 REVISION=$(curl -L -H 'Accept: application/json' https://hydra.nixos.org/eval/$JOBSETEVAL | jq .jobsetevalinputs.nixpkgs.revision)
 echo $REVISION
