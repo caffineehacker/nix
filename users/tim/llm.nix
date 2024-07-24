2dbde9c75698fe473cb4bac2cf4b3aecc372ea80
Tim Waterhouse <tim@timwaterhouse.com>
2024-07-23 17:22:08 -0700

    Fix ollama to use GPU again

--- users/tim/llm.nix
+++ users/tim/llm.nix
@@ -3,9 +3,7 @@
     services.ollama = {
       enable = true;
       acceleration = "rocm";
-      environmentVariables = {
-        HSA_OVERRIDE_GFX_VERSION = "11.0.2";
-      };
+      rocmOverrideGfx = "11.0.2";
     };
   };
 }
