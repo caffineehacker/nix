16b53204c70bc1e0c62b6ab845e79f090138eee7
Tim Waterhouse <tim@timwaterhouse.com>
2025-03-18 13:49:59 -0700

    Remove hyprland version fetching from update scripts

--- update.sh
+++ update.sh
@@ -2,9 +2,6 @@
 #!nix-shell -i bash -p nh
 set -e
 
-./find-hyprland-updates.sh
-# This will get the latest passing hydra build
-sed -i "s/^\(\s*\).*\( ### REPLACE_NIXPKGS_REV$\)/\1rev = $(./find-nixpkg-rev.sh);\2/g" flake.nix
 nh os switch --ask --update $(dirname "$0") -- -j 2 --keep-failed --keep-going
 echo "Press enter to continue..."
 read
