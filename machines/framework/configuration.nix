ce7d4651a66e56c253ef474b351ef85c7fc8ea0f
Tim Waterhouse <tim@timwaterhouse.com>
2025-07-09 09:13:34 -0700

    Update gnome service name

--- machines/framework/configuration.nix
+++ machines/framework/configuration.nix
@@ -291,8 +291,7 @@ in
   };
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.pipewire = {
     alsa = {
