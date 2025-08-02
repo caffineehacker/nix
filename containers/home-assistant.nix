f4919ffa5770da5de5d73fe6585cb4494584241f
Tim Waterhouse <tim@timwaterhouse.com>
2025-08-02 15:06:13 -0700

    Got zwave working

--- containers/home-assistant.nix
+++ containers/home-assistant.nix
@@ -7,27 +7,70 @@ in
   imports = [ (helpers.module cfg) ];
 
   config = lib.mkIf cfg.enable {
-    containers."${cfg.name}".config = { ... }: {
-      imports = [ (helpers.containerConfigModule cfg) ];
+    containers."${cfg.name}" = {
+      allowedDevices = [
+        {
+          node = "/dev/ttyACM0";
+          modifier = "rwm";
+        }
+      ];
+      bindMounts."/dev/ttyACM0" = { hostPath = "/dev/ttyACM0"; isReadOnly = false; };
+      config = { ... }: {
+        imports = [ (helpers.containerConfigModule cfg) ];
 
-      services.home-assistant = {
-        enable = true;
-        extraComponents = [
-          # Components required to complete the onboarding
-          "esphome"
-          "met"
-          "radio_browser"
-          "zwave_me"
-          "zwave_js"
-        ];
-        config = {
-          # Includes dependencies for a basic setup
-          # https://www.home-assistant.io/integrations/default_config/
-          default_config = { };
-          http = {
-            server_host = "::1";
-            trusted_proxies = [ "::1" ];
-            use_x_forwarded_for = true;
+        systemd.services.chmodDevTty = {
+          serviceConfig = {
+            Type = "oneshot";
+          };
+          wantedBy = [ "zwave-js.service" ];
+          script = ''
+            chmod a+rw /dev/ttyACM0
+          '';
+        };
+
+        system.activationScripts.changeTtyMods = {
+          text = ''
+            chmod a+rw /dev/ttyACM0
+          '';
+        };
+
+        networking.firewall.allowedTCPPorts = [ 8091 ];
+
+        services.zwave-js = {
+          enable = true;
+          serialPort = "/dev/ttyACM0";
+          # This needs to be manually created in the container
+          secretsConfigFile = "/etc/zwave_secrets";
+        };
+
+        services.zwave-js-ui = {
+          enable = true;
+          serialPort = "/dev/ttyACM0";
+          settings = {
+            HOST = "::1";
+            PORT = "8091";
+          };
+        };
+
+        services.home-assistant = {
+          enable = true;
+          extraComponents = [
+            # Components required to complete the onboarding
+            "esphome"
+            "met"
+            "radio_browser"
+            "zwave_me"
+            "zwave_js"
+          ];
+          config = {
+            # Includes dependencies for a basic setup
+            # https://www.home-assistant.io/integrations/default_config/
+            default_config = { };
+            http = {
+              server_host = "::1";
+              trusted_proxies = [ "::1" ];
+              use_x_forwarded_for = true;
+            };
           };
         };
       };
