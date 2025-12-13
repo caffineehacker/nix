b46b5c470fe6ecbe0f19752329af25d8f4d003e8
Tim Waterhouse <tim@timwaterhouse.com>
2025-12-12 16:33:27 -0800

    Make home assistant declaritive

--- containers/home-assistant.nix
+++ containers/home-assistant.nix
@@ -52,6 +52,8 @@ in
           };
         };
 
+        # NOTE: MQTT and z-wave JS need to be manually configured in the home assistant
+        # UI for some reason. They cannot be configured declaritively :-(
         services.home-assistant = {
           enable = true;
           package = (pkgs.home-assistant.override {
@@ -65,19 +67,94 @@ in
               "zwave_js"
             ];
           });
-          # Temporarily use local config until I have everything working...
-          configDir = "/etc/home-assistant";
-          config = null;
-          # config = {
-          #   # Includes dependencies for a basic setup
-          #   # https://www.home-assistant.io/integrations/default_config/
-          #   default_config = { };
-          #   http = {
-          #     server_host = "::1";
-          #     trusted_proxies = [ "::1" ];
-          #     use_x_forwarded_for = true;
-          #   };
-          # };
+          configDir = "/var/lib/haas";
+          config = {
+            default_config = { };
+            automation = "!include automations.yaml";
+            mqtt = {
+              climate =
+                let
+                  heatPump = name: baseTopic: {
+                    inherit name;
+                    device = {
+                      name = "${name} Heatpump";
+                      identifiers = [
+                        baseTopic
+                      ];
+                    };
+                    unique_id = "${baseTopic}";
+                    optimistic = true;
+                    current_temperature_topic = "${baseTopic}/status";
+                    current_temperature_template = "{{ value_json.roomTemperature }}";
+                    mode_state_topic = "${baseTopic}";
+                    mode_command_topic = "${baseTopic}/set";
+                    swing_horizontal_mode_state_topic = "${baseTopic}";
+                    swing_mode_state_topic = "${baseTopic}";
+                    temperature_state_topic = "${baseTopic}";
+                    temperature_command_topic = "${baseTopic}/set";
+                    fan_mode_state_topic = "${baseTopic}/status";
+                    fan_mode_command_topic = "${baseTopic}/set";
+                    fan_mode_command_template = "{{ {'fan': value}|to_json }}";
+                    fan_mode_state_template = "{{ value_json.fan }}";
+                    fan_modes = [
+                      "AUTO"
+                      "QUIET"
+                      "1"
+                      "2"
+                      "3"
+                      "4"
+                    ];
+                    min_temp = 61.0;
+                    mode_state_template = "{% if value_json.power == 'OFF' %}{{ 'off' }}{% else %}{{ { 'COOL': 'cool', 'HEAT': 'heat', 'DRY': 'dry', 'FAN': 'fan_only', 'AUTO': 'auto' }[value_json.mode] }}{% endif %}";
+                    mode_command_template = "{% if value == 'off' %} {{ {'power': 'OFF'}|to_json }} {% else %} {{ {'power': 'ON', 'mode': { 'cool': 'COOL', 'heat': 'HEAT', 'dry': 'DRY', 'fan_only': 'FAN', 'auto': 'AUTO' }[value] }|to_json }} {% endif %}";
+                    swing_horizontal_mode_state_template = "{{ value_json.wideVane }}";
+                    swing_horizontal_modes = [
+                      "<<"
+                      "<"
+                      "|"
+                      ">"
+                      ">>"
+                      "<>"
+                      "SWING"
+                    ];
+                    swing_mode_state_template = "{{ value_json.vane }}";
+                    swing_modes = [
+                      "AUTO"
+                      "1"
+                      "2"
+                      "3"
+                      "4"
+                      "5"
+                      "SWING"
+                    ];
+                    temperature_state_template = "{{ value_json.temperature }}";
+                    temperature_unit = "F";
+                    temperature_command_template = "{{ {'temperature': value}|to_json }}";
+                  };
+                in
+                [
+                  (heatPump "Tim's Office" "heatpump/timsoffice")
+                  (heatPump "Dining Room" "heatpump/hpdiningroom")
+                  (heatPump "Outdoor Office" "heatpump/outdooroffice")
+                  (heatPump "Family Room" "heatpump/familyroom")
+                  (heatPump "Bedroom" "heatpump/hpmasterbdrm")
+                  (heatPump "Connor's Office" "heatpump/hpconnorsroom")
+                ];
+            };
+            homeassistant = { };
+            http = {
+              server_host = "::1";
+              server_port = cfg.cloudflare.port;
+              trusted_proxies = [ "::1" ];
+              use_x_forwarded_for = true;
+              ip_ban_enabled = true;
+              login_attempts_threshold = 5;
+            };
+            lovelace = {
+              mode = "storage";
+              resources = [ ];
+            };
+          };
         };
 
         services.fail2ban = {
@@ -117,3 +194,4 @@ in
     };
   };
 }
+
