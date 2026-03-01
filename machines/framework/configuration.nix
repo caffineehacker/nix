04aab794d4021c52307a47ebe49d97f21aa11800
Tim Waterhouse <tim@timwaterhouse.com>
2026-03-01 13:54:11 -0800

    Simplify overriding for march / mcpu

--- machines/framework/configuration.nix
+++ machines/framework/configuration.nix
@@ -11,9 +11,6 @@ let
       rocmSupport = false;
     };
   };
-  nixpkgs-unoptimized-i686 = import inputs.nixpkgs {
-    system = "i686-linux";
-  };
 in
 {
   nix = {
@@ -29,227 +26,76 @@ in
     settings.show-trace = true;
   };
   nixpkgs.hostPlatform = {
+    gcc = {
+      arch = "znver4";
+      tune = "znver4";
+    };
     system = "x86_64-linux";
-    gcc.arch = "znver4";
-    gcc.tune = "znver4";
   };
   nixpkgs.config.rocmSupport = true;
   nixpkgs.overlays =
     let
-      # This comes from nixpkgs/pkgs/stdenv/adapters.nix. The primary change is that we don't make a default mkDerivationFromStdenv if stdenvSuperArgs.mkDerivationFromStdenv doesn't exist.
-      # Instead, we require the caller to have already used a method like addAttrsToDerivation which will create a default mkDerivationFromStdenv if it doesn't already exist.
-      withOldMkDerivation = stdenvSuperArgs: k: stdenvSelf:
-        let
-          mkDerivationFromStdenv-super = stdenvSuperArgs.mkDerivationFromStdenv;
-          mkDerivationSuper = mkDerivationFromStdenv-super stdenvSelf;
-        in
-        k stdenvSelf mkDerivationSuper;
-
-      # Wrap the original `mkDerivation` providing extra args to it.
-      extendMkDerivationArgs = old: f: withOldMkDerivation old (_: mkDerivationSuper: args:
-        (mkDerivationSuper args).overrideAttrs f);
-
-      removeFlagsByPrefix = prefix: flags: lib.lists.foldr (a: b: (if (lib.strings.hasPrefix prefix a) || (a == "") then b else (a + " " + b))) "" (lib.strings.splitString " " flags);
-
-      removeOFlags = flags: removeFlagsByPrefix "-O" flags;
-      removeMarchFlags = flags: removeFlagsByPrefix "-mtune=" (removeFlagsByPrefix "-march=" flags);
-      removeExistingOptimizations = flags: removeOFlags (removeMarchFlags flags);
-
-      # This is hacky, but just replacing the x86_64 package will cause the i686 package to incorrectly use x86_64-linux system.
-      # We also can't directly override the pkgsi686linux and instead have to do this...
-      useUnoptimized-x64 = super: pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized.pkgs)) // b) { } pkgs;
-      useUnoptimized-i686 = super: pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized-i686.pkgs)) // b) { } pkgs;
-      useUnoptimized = super: pkgs: if (super.stdenv.system == "x86_64-linux") then (useUnoptimized-x64 super pkgs) else (useUnoptimized-i686 super pkgs);
-
-      useUnoptimizedHaskell-x64 = super: pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized.pkgs.haskellPackages)) // b) { } pkgs;
-      useUnoptimizedHaskell-i686 = super: pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized-i686.pkgs.haskellPackages)) // b) { } pkgs;
-      useUnoptimizedHaskell = super: pkgs: {
-        haskellPackages = super.haskellPackages.override {
-          overrides = (new: old: (if (super.stdenv.system == "x86_64-linux") then (useUnoptimizedHaskell-x64 super pkgs) else (useUnoptimizedHaskell-i686 super pkgs)));
-        };
-      };
+      useUnoptimized-x64 = pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized.pkgs)) // b) { } pkgs;
+      useUnoptimized-i686 = pkgs: lib.lists.foldr (a: b: (lib.attrsets.setAttrByPath [ a ] (lib.attrsets.getAttrFromPath [ a ] nixpkgs-unoptimized.pkgs.pkgsi686linux)) // b) { } pkgs;
     in
     [
-      (self: super: {
-        # This is the same as super.withCFlags, but allows setting the attribute instead of the env for some packages
-        # We need to apply addAttrsToDerivation to ensure there is always a mkDerivationFromStdenv already.
-        stdenv = (super.addAttrsToDerivation { } super.stdenv).override (old: {
-          # Bootstrap compilers don't support lto and don't really need special flags
-          mkDerivationFromStdenv = extendMkDerivationArgs old (args:
-            let
-              enableFlto = false;
-              updateMarch = super.stdenv.system == "x86_64-linux";
-              removeFunction = if (updateMarch) then removeExistingOptimizations else removeOFlags;
-              updateFunction = flags: (removeFunction flags) + " -O3 " + (if updateMarch then " -march=znver4 -mtune=znver4 " else "") + (if enableFlto then " -flto " else "");
-            in
-            if (lib.strings.hasInfix "bootstrap" old.name) then { } else
-            (lib.attrsets.optionalAttrs (args ? "NIX_CFLAGS_COMPILE") {
-              NIX_CFLAGS_COMPILE = updateFunction (toString (args.NIX_CFLAGS_COMPILE));
-            }) // (lib.attrsets.optionalAttrs (!(args ? "NIX_CFLAGS_COMPILE") || ((args.env or { }) ? "NIX_CFLAGS_LINK")) {
-              env = (args.env or { }) // lib.attrsets.optionalAttrs (!(args ? "NIX_CFLAGS_COMPILE")) {
-                NIX_CFLAGS_COMPILE = updateFunction (toString (args.env.NIX_CFLAGS_COMPILE or ""));
-              };
-            }));
-        });
-        # Segfault in checks - 4/23/2024
-        libvorbis = super.libvorbis.override
-          (old: {
-            stdenv = super.withCFlags [ "-march=x86-64 -mtune=generic" ] old.stdenv;
-          });
-        # Compile error in AVX512 test - 4/23/2024
-        simde = super.simde.override
-          (old: {
-            stdenv = super.withCFlags [ "-march=x86-64 -mtune=generic" ] old.stdenv;
-          });
-        # A test fails due to being too slow while building everything else - 4/24/2024
-        pythonPackages.hypothesis = super.pythonPackages.hypothesis.overridePythonAttrs
-          {
-            doCheck = false;
-            pytestCheckPhase = "true";
-          };
-        pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
-          (pyfinal: pysuper: {
-            scipy = pysuper.scipy.overridePythonAttrs (oldAttrs: {
-              # Tests fail due to floating point precision due to optimizations
-              disabledTests = pysuper.scipy.disabledTests ++ [ "test_equal_bounds" "test_bug_6139" "test_roundtrip_scaling" "test_initial_step" ];
-            });
-          })
-        ];
-        # A test seg faults - 4/24/2024
-        libsndfile = super.libsndfile.override
-          (old: {
-            stdenv = old.stdenv // { hostPlatform = lib.systems.elaborate { system = "x86_64-linux"; }; };
-          });
-        # Floating point precision test failures - 4/28/2024
-        gsl = super.gsl.override
-          (old: {
-            stdenv = super.withCFlags [ "-march=x86-64" "-mtune=generic" "-mno-fma" ] old.stdenv;
-          });
-        # Floating point precision test failures - 4/28/2024
-        lib2geom = super.lib2geom.override
-          (old: {
-            stdenv = super.withCFlags [ "-march=x86-64" "-mtune=generic" "-mno-fma" ] old.stdenv;
-          });
-        # libgcrypt requires special logic on applying -O flags
-        libgcrypt = super.libgcrypt.override (old: {
-          stdenv = super.stdenv.override (old: {
-            mkDerivationFromStdenv = extendMkDerivationArgs old (args:
-              lib.attrsets.optionalAttrs (args ? "NIX_CFLAGS_COMPILE")
-                {
-                  NIX_CFLAGS_COMPILE = removeOFlags (toString (args.NIX_CFLAGS_COMPILE));
-                } // lib.attrsets.optionalAttrs (!(args ? "NIX_CFLAGS_COMPILE")) {
-                env = (args.env or { }) // {
-                  NIX_CFLAGS_COMPILE = removeOFlags (toString (args.env.NIX_CFLAGS_COMPILE));
-                };
-              });
-          });
-        });
-        # Warning as error about maybe uninitialized variables
-        bpftools = super.bpftools.override (old: {
-          stdenv = super.stdenv.override (old: {
-            mkDerivationFromStdenv = extendMkDerivationArgs old (args:
-              lib.attrsets.optionalAttrs (args ? "NIX_CFLAGS_COMPILE")
-                {
-                  NIX_CFLAGS_COMPILE = toString (args.NIX_CFLAGS_COMPILE) + " -Wno-maybe-uninitialized ";
-                } // lib.attrsets.optionalAttrs (!(args ? "NIX_CFLAGS_COMPILE")) {
-                env = (args.env or { }) // {
-                  NIX_CFLAGS_COMPILE = toString (args.env.NIX_CFLAGS_COMPILE) + " -Wno-maybe-uninitialized ";
-                };
-              });
-          });
-        });
-        # Warning as error about maybe uninitialized variables
-        libbpf = super.libbpf.override (old: {
-          stdenv = super.stdenv.override (old: {
-            mkDerivationFromStdenv = extendMkDerivationArgs old (args:
-              lib.attrsets.optionalAttrs (args ? "NIX_CFLAGS_COMPILE")
-                {
-                  NIX_CFLAGS_COMPILE = toString (args.NIX_CFLAGS_COMPILE) + " -Wno-maybe-uninitialized ";
-                } // lib.attrsets.optionalAttrs (!(args ? "NIX_CFLAGS_COMPILE")) {
-                env = (args.env or { }) // {
-                  NIX_CFLAGS_COMPILE = toString (args.env.NIX_CFLAGS_COMPILE) + " -Wno-maybe-uninitialized ";
-                };
-              });
-          });
-        });
-        # Rocblas takes forever to build and just overriding it does not update the dependency for other packages unfortuntately.
-        rocmPackages_6 = nixpkgs-unoptimized.pkgs.rocmPackages_6;
+      (final: prev: {
         # This seems to customize the rocmPackages when built with rocm support (actually it's the onnxruntime dependency) - 11/14/2025
         thunderbird = nixpkgs-without-rocm.thunderbird;
       })
-      (final: super: (useUnoptimized super [
-        # These are here because they can be very slow to build
-        "nodejs"
-        "nodejs-slim"
-        "electron"
-        # Some of these don't exist yet, but will help prevent issues when they do
-        "electron_29"
-        "electron_30"
-        "electron_31"
-        "electron_32"
-        "electron_33"
-        "electron_34"
-        "electron_35"
-        "electron_36"
-        "electron_37"
-        "electron_38"
-        "electron_39"
-        "electron-unwrapped"
-        "firefox"
-        "firefox-bin"
-        "webkitgtk"
-        "webkitgtk_4_1"
-        "webkitgtk_5_0"
-        "webkitgtk_6_0"
-        "llvm"
-        # Test failure (checkasm) - 02/05/2025
-        "dav1d"
-        # Fails to build due to expecting BPF support that is apparently not available in clang?? - 02/05/2025
-        "systemd"
-        # # Fails to build due to aggressive size checks - 02/05/2025
-        "libtpms"
-        # Fails a test for unknown reason - 02/05/2025
-        "lib2geom"
-        # Fails to compile due to format overflow - 02/05/2025
-        "efivar"
-        # Fails a test - 02/04/2025
-        "graphene"
-        # Fails to find some managed application when building dotnet - 02/04/2025
-        "ryujinx"
-        # Fails to build a dependency, openexr, that is customized so an override doesn't use the cache - 02/04/2025
-        "gst_all_1"
-        # Causes a nix parsing stack overflow when using an override for some reason - 5/22/2025
-        "easyeffects"
-        # TLS dependency fails to build - 5/22/2025
-        "pandoc"
-        # Test failures - 7/8/2025
-        "assimp"
-        # Build failures - 7/31/2025
-        "v4l-utils"
-        # Infinite recursion in call depth for nix configs - 8/12/2025
-        "php"
-        # Takes forever to build
-        "ollama"
-        # Dependencies have some issues - 10/23/2025
-        "lutris"
-        # Fails tests - 11/20/2025
-        "ffmpeg-headless"
-        "ffmpeg"
-        # Takes too long to build
-        "qtwebengine"
-        "pyside6"
-        "rustc"
-        "rustc-wrapper"
-        "clang"
-        "ghc"
-        "ryubing"
-        # Fails due to warning as error for string overflow - 02/02/2026
-        "gdb"
-      ]))
-      (final: super: (useUnoptimizedHaskell super [
-        # Test failures - 04/23/2025
-        "crypton-x509-validation"
-      ]))
+      (final: prev:
+        if prev.stdenv.system == "x86_64-linux" then
+          {
+            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+              # Fails tests - 2/27/2026
+              (pyfinal: pyprev: {
+                psycopg = pyprev.psycopg.overridePythonAttrs (oldAttrs: {
+                  # Tests fail due to timing issues
+                  disabledTests = pyprev.psycopg.disabledTests ++ [ "test_stats_connect[asyncio]" "test_stats_connect" ];
+                });
+              })
+            ];
+          } //
+          (useUnoptimized-x64 [
+            # Rocblas takes forever to build and just overriding it does not update the dependency for other packages unfortuntately.
+            "rocmPackages_6"
+            "rocmPackages"
+            # Fails due to a string overflow warning as error - 02/05/2025
+            "libtpms"
+            # Test failures - 7/8/2025
+            "assimp"
+            # Test failures - 02/22/2026
+            "gsl"
+            # Fails a test for unknown reason - 02/05/2025
+            "lib2geom"
+            # Some of these don't exist yet, but will help prevent issues when they do
+            "electron_29"
+            "electron_30"
+            "electron_31"
+            "electron_32"
+            "electron_33"
+            "electron_34"
+            "electron_35"
+            "electron_36"
+            "electron_37"
+            "electron_38"
+            "electron_39"
+            "electron_40"
+            "electron_41"
+            "electron-unwrapped"
+            "webkitgtk"
+            "webkitgtk_4_1"
+            "webkitgtk_5_0"
+            "webkitgtk_6_0"
+            # Fails due to variable type conversion issues - 02/23/2026
+            "mesa"
+            # # Complains of symbols in wrong format - 2/23/2026
+            # "swtpm"
+            # Fails to build due to variable sizes - 2/23/2026
+            "simde"
+            # Test failures - 3/1/2026
+            "valkey"
+          ]) else (useUnoptimized-i686 [ ]))
     ];
 
   imports = [
