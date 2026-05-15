6be780656d15078124f519203abca48abb2fa09b
Tim Waterhouse <tim@timwaterhouse.com>
2026-05-15 13:07:02 -0700

    Remove TPM to boot faster

--- machines/homelab/configuration.nix
+++ machines/homelab/configuration.nix
@@ -12,19 +12,34 @@
     system = "x86_64-linux";
   };
 
-  boot.loader.systemd-boot.configurationLimit = 5;
-  boot.loader.systemd-boot.memtest86.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.systemd.enable = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelParams = [
-    # I get random hangs / crashes, trying lots of things
-    "acpi=off"
-    "noapic"
-    "processor.max_cstate=2"
-  ];
+  boot = {
+    loader = {
+      systemd-boot = {
+        configurationLimit = 5;
+        memtest86.enable = true;
+        enable = true;
+      };
+      efi.canTouchEfiVariables = true;
+      grub.device = "nodev";
+    };
+    initrd = {
+      systemd = {
+        enable = true;
+        tpm2.enable = false;
+      };
+    };
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelParams = [
+      # I get random hangs / crashes, trying lots of things
+      "acpi=off"
+      "noapic"
+      "processor.max_cstate=2"
+    ];
+    kernel.sysctl = {
+      "vm.dirty_ratio" = 10;
+      "vm.dirty_background_ratio" = 5;
+    };
+  };
 
   tw.services.ssh.enable = true;
 
@@ -40,11 +55,6 @@
 
   services.fwupd.enable = true;
   services.power-profiles-daemon.enable = true;
-
-  boot.kernel.sysctl = {
-    "vm.dirty_ratio" = 10;
-    "vm.dirty_background_ratio" = 5;
-  };
 
   # Trim ssd for longer life and better storage
   services.fstrim.enable = true;
