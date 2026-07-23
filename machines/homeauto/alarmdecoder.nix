f7702f3dd569e14f2a5f007a618f5b8613632eb2
Tim Waterhouse <tim@timwaterhouse.com>
2026-07-23 14:46:38 -0700

    Ensure alarm decoder waits for mqtt

--- machines/homeauto/alarmdecoder.nix
+++ machines/homeauto/alarmdecoder.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }: {
   systemd.services.alarm-to-mqtt = {
     wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    wants = [ "mosquitto.service" ];
+    after = [ "network.target" "mosquitto.service" ];
     description = "Connects MQTT server and AlarmDecoder";
     serviceConfig = {
+      # To ensure mosquitto can fully start
+      ExecStartPre = "/bin/sleep 30";
       ExecStart =
         let
           alarm-to-mqtt = pkgs.writers.writePython3Bin "alarm-to-mqtt.py" { libraries = with pkgs.python3Packages; [ alarmdecoder paho-mqtt ]; } (
