6d4f4b60a78284dc64b71941a328d806ab9323de
Tim Waterhouse <tim@timwaterhouse.com>
2026-07-28 09:34:13 -0700

    Fix alarmdecoder deploay

--- machines/homeauto/alarmdecoder.nix
+++ machines/homeauto/alarmdecoder.nix
@@ -5,8 +5,8 @@
     after = [ "network.target" "mosquitto.service" ];
     description = "Connects MQTT server and AlarmDecoder";
     serviceConfig = {
-      # To ensure mosquitto can fully start
-      ExecStartPre = "/bin/sleep 30";
+      # # To ensure mosquitto can fully start
+      # ExecStartPre = "/bin/sleep 30";
       ExecStart =
         let
           alarm-to-mqtt = pkgs.writers.writePython3Bin "alarm-to-mqtt.py" { libraries = with pkgs.python3Packages; [ alarmdecoder paho-mqtt ]; } (
